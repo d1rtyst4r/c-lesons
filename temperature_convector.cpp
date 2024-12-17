ce9313c430ed86b10f7b035e1e64462a8ba3a4eb
Ruslans <33938604+d1rtyst4r@users.noreply.github.com>
2024-12-17 21:43:39 +0200

    1.3 leson

--- temperature_convector.cpp
+++ temperature_convector.cpp
@@ -18,9 +18,8 @@ main()
         fahr += step;
     }
     */
-   for (fahr = lower; fahr <= upper; fahr += step){
-    celcsius =  (5.0/9.0) * (fahr-32.0);
-    printf("%3.0f\t%6.2f\n", fahr, celcsius);
+   for (fahr = 300; fahr >= 0; fahr -= 20){
+    printf("%3.0f\t%6.2f\n", fahr, (5.0/9.0) * (fahr-32.0));
    }
     
 }
